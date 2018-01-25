e8b08c728f32a5662ad6f5cf0a5dac336fc751df
Jacques le Roux <djiaak@gmail.com>
2018-01-24 19:35:17 -0500

    Changed server port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const app = express();
+const port = 8666;
 
 app.use(express.static('dist'));
-app.listen(80, () => console.log('App running on port 80'));
+app.listen(port, () => console.log(`App running on port ${port}`));
