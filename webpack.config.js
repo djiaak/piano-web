e3dd0a27e3e7df4c3bc680805ab1d071d72fe939
Jacques le Roux <djiaak@gmail.com>
2019-01-31 21:50:00 -0500

    Use script-loader for bridge compiled scripts

--- webpack.config.js
+++ webpack.config.js
@@ -9,60 +9,73 @@ const webpack = require('webpack');
 module.exports = {
   mode: 'development',
   resolve: {
-    extensions: ['.js', '.jsx', '.scss']
+    extensions: ['.js', '.jsx', '.scss'],
   },
   entry: './client/index.js',
   output: {
     path: path.resolve('dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
   },
   devtool: 'sourcemap',
   module: {
-    rules: [{ 
-        test: /\.jsx?$/, 
+    rules: [
+      {
+        test: /\.jsx?$/,
         exclude: /(node_modules|MidiSheetMusic)/,
-        use: [{
-          loader: 'babel-loader', 
-          query: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
-        }]
-      }, {
+        use: [
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+              plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+          },
+        ],
+      },
+      {
         test: /\.scss$/,
-        use: [{
-          loader: "style-loader"
-        }, {
-          loader: "css-loader"
-        }, {
-          loader: "sass-loader"
-        }]
-      }, { 
-        include: path.resolve(__dirname, 'client/external/MidiSheetMusic/build'),
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+      {
+        include: path.resolve(
+          __dirname,
+          'client/external/MidiSheetMusic/build',
+        ),
         test: /\.js$/,
-        parser: { system: false }
-      }, {
+        parser: { system: false },
+        use: [ 'script-loader' ]
+      },
+      {
         test: /\.(png|jpg|gif|mid)$/,
         use: [
           {
             loader: 'file-loader',
-            options: {}  
-          }
-        ]
-      }
-    ]
+            options: {},
+          },
+        ],
+      },
+    ],
   },
-  plugins: [ new HtmlWebpackPlugin({
-    template: './client/index.html',
-    filename: 'index.html',
-    inject: 'body'
-  }), new webpack.ContextReplacementPlugin(/./g, ctx => {
-    //TODO fix 'Critical dependency: the request of a dependency is an expression' error
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
   node: {
     fs: 'empty',
     net: 'empty',
     tls: 'empty',
-    graphics: 'empty'
-  }
-}
+  },
+};
